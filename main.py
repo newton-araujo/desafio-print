805be560e8f98cb903a4d498c1a67e38210150ff
newton-araujo <27274177-newton-araujo@users.noreply.replit.com>
2023-12-23 04:04:41 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,8 @@
+"""
+No 1° desafio do curso de python da "Udemy", foi proposto usarmos a função "print",
+com a seguinte frase "Olá mundo Python".
+"""
+
+frase = 'Olá mundo Python!'
+
+print(frase)
